8ab13cbceb19cea287ef20384de1c1440751eb63
ytcxl <96937429+ytcxl@users.noreply.github.com>
2022-06-03 11:18:29 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "cfa1a743-a208-4c4b-9a60-cc4f785563a6",
                         "alterId": 0
                     }
                 ],
